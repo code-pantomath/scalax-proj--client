b5e1d92d8cd01a63e96dc5a673e74a34a51a5d84
Code-Pantomath <72104746+code-pantomath@users.noreply.github.com>
2022-12-09 02:02:23 +0300

    Update CONSTANTS.cs

--- CONSTANTS.cs
+++ CONSTANTS.cs
@@ -1,13 +1,11 @@
-﻿
-
-namespace Scalax_client.CONSTANTS;
+﻿namespace Scalax_client.CONSTANTS;
 
 public class CONSTANTS
 {
-    private static readonly bool isDevEnv = false;
+    private static readonly bool isDevEnv = true;
 
     public static readonly string exeFilePath = Environment.CurrentDirectory;
     public static readonly string tmpFilePath = Path.Combine(exeFilePath, "tmp.txt");
 
-    public static readonly string SERVER_ENDPOINT_URL = isDevEnv ? "http://127.0.0.1:5000" : "https://site--scalax-server-01--n5gfpf86nbvs.code.run";
+    public static readonly string SERVER_ENDPOINT_URL = isDevEnv ? "http://127.0.0.1:5000" : "<PROD_SERVER_URL>";
 }
